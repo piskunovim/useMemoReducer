1cd567586d2f456e466947c7178b71b3e02e4ce2
Vladimir Grigoryev <vladimir_grigoryev@hotmail.com>
2023-07-07 18:57:45 +0300

    rewrite devtools tests

--- tests/devTools/devTools.test.tsx
+++ tests/devTools/devTools.test.tsx
@@ -1,60 +1,35 @@
-import React from 'react';
-import { render, act, cleanup } from '@testing-library/react';
+import { renderHook } from '@testing-library/react';
 
 import { useMemoReducer } from '../../src';
+import { getCurrentHookValue } from '../helpers';
 import * as useReduxDevtools from '../../src/hooks/useReduxDevtools/useReduxDevtools';
-import { Dispatch } from '../../src/models';
-
-type CustomDispatch<S, A> = {
-  current: Dispatch<S, A> | null;
-};
 
 jest.mock('../../src/hooks/useReduxDevtools/useReduxDevtools');
+jest.mock('../../src/hooks/useReduxDevtools/helpers');
 
 describe('devTools', () => {
-  const customDispatchRef: CustomDispatch<any, any> = { current: null };
-
-  afterEach(() => {
-    cleanup();
-    customDispatchRef.current = null;
-  });
-
-  it('should dispatch action to devtools when devtools are enabled', () => {
-    enum Action {
-      MOCK_ACTION = 'MOCK_ACTION',
-    }
-    const reducer = jest.fn((state) => state);
-    const initialState = {};
-
-    // Mock the dispatchToDevtools function
+  it('should dispatch an action to the Redux Dev Tools Extension when one is enabled', () => {
     const mockDispatchToDevtools = jest.fn();
 
-    // Mock implementation for useCreateReduxDevtools
     (useReduxDevtools.useCreateReduxDevtools as jest.Mock).mockImplementation(() => ({
       devtoolsEnabled: () => true,
       dispatchToDevtools: mockDispatchToDevtools,
     }));
 
-    const DevToolsTestComponent = () => {
-      const [, customDispatch] = useMemoReducer(reducer, initialState);
+    const [, customDispatch] = getCurrentHookValue(
+      renderHook(() =>
+        useMemoReducer(
+          jest.fn((state) => state),
+          {},
+        ),
+      ),
+    );
 
-      // Expose customDispatch for the test
-      React.useEffect(() => {
-        customDispatchRef.current = customDispatch;
-      }, [customDispatch]);
+    const Action = { type: 'MOCK_ACTION' };
 
-      return null;
-    };
+    customDispatch(Action);
 
-    render(<DevToolsTestComponent />);
-
-    const mockAction = { type: Action.MOCK_ACTION };
-
-    act(() => {
-      return customDispatchRef.current && customDispatchRef.current(mockAction);
-    });
-
-    // Assert that dispatchToDevtools was called with the correct action
-    expect(mockDispatchToDevtools).toHaveBeenCalledWith(mockAction);
+    expect(mockDispatchToDevtools).toHaveBeenCalledWith(Action);
+    expect(mockDispatchToDevtools).toHaveBeenCalledTimes(1);
   });
 });
